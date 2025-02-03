4a6b98add0650cd96124ed6ae6a0ada73590048d
FixFever <fixfever@ya.ru>
2025-02-04 01:41:34 +0300

    Move torrupd files

--- qbittorrent/TorrUpd.ps1
+++
@@ -1,5 +0,0 @@
-# script for scheduler:
-# pwsh -WindowStyle hidden -file "C:\git\server\qbittorrent\TorrUpd.ps1"
-# set workdir C:\git\server
-
-docker-compose -f docker-compose-torr-upd.yml up -d
---
+++ torrupd/TorrUpd.ps1
@@ -0,0 +1,5 @@
+# script for scheduler:
+# pwsh -WindowStyle hidden -file "C:\git\server\torrupd\TorrUpd.ps1"
+# set workdir C:\git\server\torrupd
+
+docker-compose up -d
