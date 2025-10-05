eac7f9ba640a2a0f9ad5b6b156cc41a43da93c7f
FixFever <fixfever@ya.ru>
2025-10-05 04:52:14 +0300

    Wait until wsl started before attach usb

--- OnStartup.cmd
+++ OnStartup.cmd
@@ -1,8 +1,16 @@
 REM add link to %appdata%\Microsoft\Windows\Start Menu\Programs\Startup
 
+REM Send notify to telegram
+pwsh -command `Invoke-WebRequest -URI ($Env:TELEGRAM_REPORT_URL + \"Server is up\");`
+
 REM USB CO2 meter attach to WSL
 REM https://github.com/dorssel/usbipd-win/issues/798
+REM Wait until wsl started
+TIMEOUT 180
+:check_wsl
+wsl --list >nul 2>&1
+if %errorlevel% neq 0 (
+    timeout /t 1 >nul
+    goto check_wsl
+)
 usbipd attach --wsl --busid 1-6
-
-REM Send notify to telegram
-pwsh -command `Invoke-WebRequest -URI ($Env:TELEGRAM_REPORT_URL + \"Server is up\");`;
