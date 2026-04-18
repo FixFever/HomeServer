ea6fb5b0eba7041a34ae8fb3b1881d6b6e11b43a
FixFever <fixfever@ya.ru>
2026-04-18 23:59:31 +0300

    Update SwitchQBitTorrentAltSpeed.ps1

--- tautulli/SwitchQBitTorrentAltSpeed.ps1
+++ tautulli/SwitchQBitTorrentAltSpeed.ps1
@@ -1,1 +1,9 @@
-qbt global limit alternative --set $args[0] --username $Env:QBITTORRENT_USER --password $Env:QBITTORRENT_PASSWORD --url http://localhost:8080/
+$pause = [bool]::Parse($args[0])
+
+if ($pause) {
+	qbt global limit alternative --set true --username $Env:QBITTORRENT_USER --password $Env:QBITTORRENT_PASSWORD --url http://localhost:8080/
+    # qbt torrent pause all --username $Env:QBITTORRENT_USER --password $Env:QBITTORRENT_PASSWORD --url http://localhost:8080/
+} else {
+	qbt global limit alternative --set false --username $Env:QBITTORRENT_USER --password $Env:QBITTORRENT_PASSWORD --url http://localhost:8080/
+    # qbt torrent resume all --username $Env:QBITTORRENT_USER --password $Env:QBITTORRENT_PASSWORD --url http://localhost:8080/
+}
